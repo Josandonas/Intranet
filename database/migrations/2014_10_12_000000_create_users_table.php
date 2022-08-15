1929f1dbd545b4fe5407d79f1b6c7363d1dc4f7b
josandonas <josandonas@outlook.com>
2022-08-15 05:42:27 -0400

    precisa de melhorias

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,11 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->string('dateBorn')->nullable();
+            $table->integerIncrements('numberCard')->unique();
+            $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
